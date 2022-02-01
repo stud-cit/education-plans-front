034b6fbe77439c972c1aad0db020aeecf8906b90
Maksim996 <12usa34airbornE@gmail.com>
2022-02-01 16:02:05 +0200

    Create (Layout.vue)

---
+++ src/views/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <navbar/>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    import Navbar from "../components/Navbar/Navbar";
+    export default {
+        components: {Navbar},
+    }
+</script>
+
+<style scoped>
+
+</style>
