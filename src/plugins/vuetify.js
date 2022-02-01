2e6b17ef03aa15f5298a2baaf1018518f88e41ae
Maksim996 <12usa34airbornE@gmail.com>
2022-02-01 16:02:59 +0200

    Update (vuetify.js) add color themes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,7 +11,7 @@ export default new Vuetify({
       },
     themes: {
       light: {
-        primary: '#007BFF',
+        primary: '#001f5a',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
