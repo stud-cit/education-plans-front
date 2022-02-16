aefea8bc6404a2c502b0998852070df70e6a2a87
Maksim996 <12usa34airbornE@gmail.com>
2022-02-16 16:32:32 +0200

    Update (vue.config.js) set alias

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path');
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+        '@c': path.resolve(__dirname, 'src/components'),
+      },
+    },
+  },
 }
