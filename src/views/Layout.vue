ed12219ad16d8a7f88af9aab89a5123c4e7b9cd2
Maksim996 <12usa34airbornE@gmail.com>
2022-02-16 16:37:49 +0200

    Update (Layout.vue) add alias import

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-    import Navbar from "../components/Navbar/Navbar";
+    import Navbar from "@c/Navbar/Navbar";
     export default {
         components: {Navbar},
     }
