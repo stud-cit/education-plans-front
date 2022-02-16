ddcae60a4c59fd7ed323898755446b999f4fe130
Maksim996 <12usa34airbornE@gmail.com>
2022-02-16 16:40:27 +0200

    Update (Plans.vue) add axios

--- src/views/pages/Plans.vue
+++ src/views/pages/Plans.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>
-    Плани
-  </div>
+  <v-container>
+    <plans-table :items="plans.items" :total="plans.total" :loading="plans.loader"  @update="getPlansList" />
+  </v-container>
 </template>
-
 <script>
-  export default {}
+  import api from '@/api';
+  import {API} from '@/api/constants-api'
+  import PlansTable from "@c/Tables/PlansTable";
+
+  export default {
+    name: 'Plans',
+    components: {PlansTable},
+    data() {
+      return {
+        total: 0,
+        plans: {
+          items: [],
+          total: 0,
+          loader: true,
+        }
+      }
+    },
+    methods: {
+      getPlansList(options) {
+        this.apiPlansList(options).then(response => {
+        const {data} = response;
+        this.plans.items =  data.data;
+        this.plans.total = data.meta.total;
+        this.plans.loader = false;
+        })
+      },
+      apiPlansList (options) {
+        this.plans.loader = true;
+
+        const data = {
+          itemsPerPage: options.itemsPerPage,
+          page: options.page
+        };
+
+        return api.get(API.LIST_PLANS, data);
+      },
+    }
+  }
 </script>
