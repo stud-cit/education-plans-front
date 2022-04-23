1f8e0497242d6caffa8ebd1c4a706d6a365688ed
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2022-04-23 19:22:54 +0300

    fix (Settiings) grid

--- src/views/pages/settings/Settings.vue
+++ src/views/pages/settings/Settings.vue
@@ -1,26 +1,30 @@
 <template>
   <v-container>
-    <v-row align="center" justify="space-around">
-      <v-card class="mt-5 mb-8">
-        <v-card-text>
-          <p class="text-h6 text--primary">Редактор обмежень</p>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn depressed href="/settings/restriction-editor">
-            Редактор обмежень
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-      <v-card class="mt-5 mb-8">
-        <v-card-text>
-          <p class="text-h6 text--primary">Термін навчання</p>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn depressed href="/settings/study-term">
-            Термін навчання
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-row align="center" justify="start">
+      <v-col cols=12 sm=6 md=4 lg=2>
+        <v-card>
+          <v-card-text>
+            <p class="text-h6 text--primary">Редактор обмежень</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn depressed block href="/settings/restriction-editor">
+              Налаштувати
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols=12 sm=6 md=4 lg=2>
+        <v-card>
+          <v-card-text>
+            <p class="text-h6 text--primary">Термін навчання</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn depressed block href="/settings/study-term">
+              Налаштувати
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
