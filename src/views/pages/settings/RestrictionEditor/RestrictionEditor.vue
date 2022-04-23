32f8ad9f855001d2378ab698b30aa211f57fa316
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2022-04-23 20:26:07 +0300

    add(RestrictionEditor) edit value validation

--- src/views/pages/settings/RestrictionEditor/RestrictionEditor.vue
+++ src/views/pages/settings/RestrictionEditor/RestrictionEditor.vue
@@ -4,6 +4,7 @@
       :headers="headers"
       :items="items"
       :loading="loader"
+      loading-text="Loading... Please wait"
       class="elevation-1"
       hide-default-footer
     >
@@ -11,11 +12,49 @@
         {{ index + 1 }}
       </template>
 
+      <template v-slot:item.value="props">
+        <v-edit-dialog
+          :return-value.sync="props.item.value"
+          large
+          persistent
+          cancel-text='Відміна'
+          save-text='Зберегти'
+          @save="save(props.item.id, props.item.value)"
+          @open="open"
+        >
+          <div>{{ props.item.value }}</div>
+          <template v-slot:input>
+            <div class="mt-4 text-h6">Редагування</div>
+            <v-text-field
+              v-model="props.item.value"
+              :rules="[required, number, max3digits]"
+              label="Редагування"
+              single-line
+              counter
+              autofocus
+            ></v-text-field>
+          </template>
+        </v-edit-dialog>
+      </template>
+
+      <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
+        {{ snackText }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn v-bind="attrs" text @click="snack = false"> Закрити </v-btn>
+        </template>
+      </v-snackbar>
+
       <template v-slot:item.actions="{ item }">
         <v-icon small class="mr-2" color="primary" @click="edit(item.id)">
           mdi-square-edit-outline
         </v-icon>
-        <v-icon small class="mr-2" color="red" @click="deleteItem(item.id, item.title)">
+        <v-icon
+          small
+          class="mr-2"
+          color="red"
+          @click="deleteItem(item.id, item.title)"
+        >
           mdi-trash-can-outline
         </v-icon>
       </template>
@@ -25,15 +64,15 @@
       <template v-slot:activator="{ on, attrs }">
         <v-fab-transition>
           <v-btn
-              color="primary"
-              dark
-              fixed
-              bottom
-              right
-              fab
-              v-bind="attrs"
-              v-on="on"
-              :to="{name: 'RestrictCreate'}"
+            color="primary"
+            dark
+            fixed
+            bottom
+            right
+            fab
+            v-bind="attrs"
+            v-on="on"
+            :to="{ name: 'RestrictCreate' }"
           >
             <v-icon>mdi-plus</v-icon>
           </v-btn>
@@ -41,7 +80,6 @@
       </template>
       <span>Додати налаштування</span>
     </v-tooltip>
-
   </v-container>
 </template>
 
@@ -54,7 +92,13 @@ export default {
 
   data() {
     return {
+      snack: false,
+      snackColor: '',
+      snackText: '',
       loader: false,
+      number: v => Number.isInteger(v) ? true :  'Тільки цілі числа!', 
+      required: v => !!v || 'Поле обов\'язкове!', 
+      max3digits: v => v.length <= 3 || 'Максимум 3 цифри!',
       items: [],
       headers: [
         { text: "№", value: "index", sortable: false },
@@ -84,6 +128,21 @@ export default {
     edit(id) {
       this.$router.push({name: 'RestrictEdit', params: { id }});
     },
+    save(id, value) {
+      const options = { value: value };
+      api.put(`${API.SETTINGS}${id}`, options).then((response) => {
+        const { message } = response.data;
+        this.snack = true;
+        this.snackColor = 'success';
+        this.snackText = message;
+       });
+    },
+
+    open () {
+      this.snack = true
+      this.snackColor = 'info'
+      this.snackText = 'Dialog opened'
+    },
 
     deleteItem(id, title) {
       this.$swal.fire({
@@ -94,7 +153,7 @@ export default {
         denyButtonText: `Ні`,
       }).then((result) => {
         if (result.isConfirmed) {
-          return api.destroy(API.SETTINGS + id);   
+          return api.destroy(API.SETTINGS + id);
         }
       })
     },
