23fc365898b8a786f4a95b158cda3ebd4e3a832e
Maksim996 <12usa34airbornE@gmail.com>
2022-05-04 13:46:51 +0300

    Update (Settings.vue) add selective disciplines

--- src/views/pages/settings/Settings.vue
+++ src/views/pages/settings/Settings.vue
@@ -25,6 +25,7 @@ export default {
         { title: 'Форми навчання', to: 'FormStudy' },
         { title: 'Форма організації навчання', to: 'FormOrganization' },
         { title: 'Користувачі', to: 'SettingUsers' },
+        { title: 'Вибіркові дисципліни', to: 'SelectiveDisciplines' },
       ],
     };
   },
