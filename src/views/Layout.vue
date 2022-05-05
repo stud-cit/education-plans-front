5bdf70938f68afedeb452d3cfec7641aed0f926c
Maksim996 <12usa34airbornE@gmail.com>
2022-05-05 22:12:47 +0300

    Update ( Layout.vue ) add breadCrumbs

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>
-        <navbar/>
-        <router-view/>
-    </div>
+  <div>
+    <navbar/>
+    <bread-crumbs/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
-    import Navbar from "@c/Navbar/Navbar";
-    export default {
-        components: {Navbar},
-    }
+import Navbar from "@c/Navbar/Navbar";
+import BreadCrumbs from "@c/Navbar/BreadCrumbs";
+
+export default {
+  components: {BreadCrumbs, Navbar},
+}
 </script>
 
 <style scoped>
