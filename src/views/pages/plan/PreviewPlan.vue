f65cc484a016bbd4b341f88e9e70d3ccdd77e1f6
Maksim996 <12usa34airbornE@gmail.com>
2022-06-02 12:28:29 +0300

    test xlsx

--- src/views/pages/plan/PreviewPlan.vue
+++ src/views/pages/plan/PreviewPlan.vue
@@ -1,6 +1,30 @@
 <template>
   <v-container>
     Preview
+    <div v-if="plan">
+      <table ref="exportable_table" width="100%">
+        <tbody >
+        <tr>
+          <td colspan="3" style="text-align: center">
+            {{plan.title}}
+          </td>
+        </tr>
+        <tr>
+          <td>Row 2 - Column 1</td>
+          <td>Row 2 - Column 2</td>
+          <td>Row 2 - Column 3</td>
+        </tr>
+        <tr>
+          <td>Row 3 - Column 1</td>
+          <td>Row 3 - Column 2</td>
+          <td>Row 3 - Column 3</td>
+        </tr>
+        </tbody>
+      </table>
+      <input type="text" v-model="name">
+      <button @click="ExportExcel('xls')">Export table to excel xls</button>
+      <button @click="ExportExcel('xlsx')">Export table to excel xlsx</button>
+    </div>
 
 
 
@@ -11,12 +35,18 @@
 </template>
 
 <script>
+// import * as XLSX from 'xlsx/xlsx.mjs';
+// import { writeFileXLSX as XLSX } from "xlsx";
+import * as XLSX from 'xlsx/xlsx.mjs';
+// import XLSX from 'xlsx'
 import {mapGetters} from "vuex";
 
 export default {
   name: "PreviewPlan",
   data() {
-    return {}
+    return {
+      name: 'test'
+    }
   },
   computed: {
     ...mapGetters({
@@ -32,6 +62,14 @@ export default {
       if (id) {
         this.$store.dispatch('plans/show', id)
       }
+    },
+    ExportExcel(type, fn, dl) {
+      var elt = this.$refs.exportable_table;
+
+      var wb = XLSX.utils.table_to_book(elt, {sheet:"Sheet JS"});
+      return dl ?
+        XLSX.write(wb, {bookType:type, bookSST:true, type: 'base64'}) :
+        XLSX.writeFile(wb, fn || ((this.name + '.'|| 'SheetJSTableExport.') + (type || 'xlsx')));
     }
   }
 }
