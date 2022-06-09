28f39c79454d731798f1b30411076ad15be1009c
Maksim996 <12usa34airbornE@gmail.com>
2022-06-09 23:40:55 +0300

    Create ( SemesterHeaderTable.vue )

---
+++ src/components/Tables/PreviewTablePlan/SemesterHeaderTable.vue
@@ -0,0 +1,90 @@
+<template>
+  <thead>
+    <tr class="table-subtitle">
+      <td class="border-table" :colspan="14 + plan.study_term.semesters">
+        V. ПЛАН НАВЧАЛЬНОГО ПРОЦЕСУ
+      </td>
+    </tr>
+    <tr>
+      <td class="border-table" rowspan="6">№</td>
+      <td class="border-table" rowspan="6">Назви навчальних дисциплін</td>
+      <td class="border-table" rowspan="1" colspan="3">Розподіл контрольних заходів за семестрами</td>
+      <td class="border-table" rowspan="6">Кількість кредитів ЄКТС</td>
+      <td class="border-table" rowspan="1" colspan="6">Кількість годин</td>
+      <td class="border-table" rowspan="1" :colspan="plan.study_term.semesters">
+        Розподіл годин на тиждень за курсами, семестрами і модульними атестаційними циклами
+      </td>
+      <td class="border-table" rowspan="6">Кафедра викладання</td>
+      <td class="border-table" rowspan="6">Потоки</td>
+    </tr>
+    <tr>
+      <td class="border-table" rowspan="5">Екзамени</td>
+      <td class="border-table" rowspan="5">Заліки</td>
+      <td class="border-table" rowspan="5">Індивідуальні завдання</td>
+
+      <td class="border-table" rowspan="5">загальний обсяг</td>
+      <td class="border-table" rowspan="1" colspan="4">аудиторних</td>
+      <td class="border-table" rowspan="5">самостійна робота</td>
+
+      <td class="border-table" v-for="course in plan.study_term.course"
+          rowspan="1"
+          :colspan="plan.study_term.semesters % plan.study_term.course > 0 && course ===  plan.study_term.course ? 1 : 2"
+          :key="'course_' + course">
+        {{course}} курс
+      </td>
+    </tr>
+    <tr>
+      <td class="border-table" rowspan="4">всього</td>
+      <td class="border-table" rowspan="1" colspan="3">у тому числі:</td>
+      <td class="border-table" rowspan="1" :colspan="plan.study_term.semesters">Семестри</td>
+    </tr>
+    <tr>
+      <td class="border-table" colspan="3" rowspan="1"></td>
+      <td class="border-table" v-for="semester in plan.study_term.semesters" colspan="1" :key="'semester_' + semester">
+        {{semester}}
+      </td>
+    </tr>
+    <tr>
+      <td class="border-table" rowspan="2">лекції</td>
+      <td class="border-table" rowspan="2">практичні, семінарські</td>
+      <td class="border-table" rowspan="2">лабораторні</td>
+      <td class="border-table" rowspan="1" :colspan="plan.study_term.semesters">Кількість тижнів у модульному атестаційному циклі</td>
+    </tr>
+    <tr>
+      <template v-for="semester in plan.study_term.semesters">
+        <td class="border-table" rowspan="1" colspan="1" :key="semester">
+          <template
+            v-if="getMaxHour(semester, plan.hours_weeks_semesters)"
+          >
+            {{getMaxHour(semester, plan.hours_weeks_semesters).hour}}
+          </template>
+        </td>
+      </template>
+    </tr>
+    <tr>
+      <td class="border-table" v-for="item in 14 + plan.study_term.semesters" :key="item">
+        {{item}}
+      </td>
+    </tr>
+  </thead>
+</template>
+
+<script>
+export default {
+  name: "SemesterHeaderTable",
+  props: {
+    plan: {
+     required: true,
+    }
+  },
+  methods: {
+    getMaxHour(semester, obj) {
+      return obj.find((item) => item.semester === semester)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
