69dadb8b7d0b40cc3a24555e4e2dd639b9539cd0
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2022-08-28 18:54:02 +0300

    fix (Forbidden page) align content

--- src/views/Forbidden.vue
+++ src/views/Forbidden.vue
@@ -1,26 +1,32 @@
 <template>
-  <div>
-    <div class="d-flex flex-column justify-center">
-    <h1 class="align-self-center big-text">403</h1>
-    <p class="align-self-center">Доступ заборонено.</p>
+  <div class="body">
+    <h1 class="big-text">403</h1>
+    <p>Доступ заборонено.</p>
     <v-btn
         text
         color="primary"
-        class="align-self-center ma-10"
+        class="ma-10"
         href="https://cabinet.sumdu.edu.ua/" >
         Повернутись на головну
       </v-btn>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'Forbidden',   
+  name: 'Forbidden',
 }
 </script>
 
 <style scoped>
+.body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 .big-text {
   font-size: 56px;
   color: #0c3754;
