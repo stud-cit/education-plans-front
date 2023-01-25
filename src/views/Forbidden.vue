1aad4d728875e03e3e4289625c5e3593c3132336
Maksim996 <12usa34airbornE@gmail.com>
2023-01-25 10:41:47 +0200

    FIx (Forbidden.vue) back up add btn

--- src/views/Forbidden.vue
+++ src/views/Forbidden.vue
@@ -2,13 +2,20 @@
   <div class="body">
     <h1 class="big-text">403</h1>
     <p>Доступ заборонено.</p>
-    <v-btn
-        text
-        color="primary"
-        class="ma-10"
-        href="/" >
-        Повернутись на головну
-      </v-btn>
+      <div class="d-flex align-center ma-10">
+        <v-btn
+          text
+          color="primary"
+          href="/" >
+          Повернутись на головну
+        </v-btn>
+        <v-btn
+          text
+          color="primary"
+          href="https://cabinet.sumdu.edu.ua/" >
+          Повернутись до кабінету
+        </v-btn>
+      </div>
     </div>
 </template>
 
