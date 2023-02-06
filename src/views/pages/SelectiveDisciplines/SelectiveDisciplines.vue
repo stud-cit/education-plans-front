9124db43dd0339cdf4cb844656b3d837dada25e5
Maksim996 <12usa34airbornE@gmail.com>
2023-02-06 15:20:50 +0200

    Update (SelectiveDisciplines.vue) add tooltip

--- src/views/pages/SelectiveDisciplines/SelectiveDisciplines.vue
+++ src/views/pages/SelectiveDisciplines/SelectiveDisciplines.vue
@@ -30,7 +30,9 @@
         <!--        <v-icon v-if="!item.edit" small class="mr-2" color="primary" @click="item.edit = true">-->
         <!--          mdi-square-edit-outline-->
         <!--        </v-icon>-->
-        <v-icon small class="mr-2 cursor-pointer" color="primary" > mdi-eye </v-icon>
+        <btn-tooltip tooltip="Перегляд">
+          <v-icon small class="mr-2 cursor-pointer" color="primary" > mdi-eye </v-icon>
+        </btn-tooltip>
       </template>
     </v-data-table>
   </v-container>
