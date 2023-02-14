31186b11578320efb220d8a9eb4a67247f978a83
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2023-02-14 14:09:20 +0200

    add loader to action copy plan

--- src/store/modules/plans/actions.js
+++ src/store/modules/plans/actions.js
@@ -1,81 +1,80 @@
 import api from '@/api';
-import {API, ALLOWED_REQUEST_PARAMETERS} from '@/api/constants-api'
-import GlobalMixin from "../../../mixins/GlobalMixin";
-
+import { API, ALLOWED_REQUEST_PARAMETERS } from '@/api/constants-api';
+import GlobalMixin from '../../../mixins/GlobalMixin';
 
 export const getListPlans = (context, payload) => {
   const options = GlobalMixin.methods.GlobalHandlingRequestParameters(ALLOWED_REQUEST_PARAMETERS.GET_PLANS, payload);
   context.commit('SET_LOADING', true);
-  api.get(API.PLANS, options, {showLoader: true}).then(({data}) => {
+  api.get(API.PLANS, options, { showLoader: true }).then(({ data }) => {
     context.commit('SET_PLANS', data);
     context.commit('SET_LOADING', false);
   });
-}
+};
 
 export const destroy = (context, id) => {
   api.destroy(API.PLANS, id).then(() => {
     context.dispatch('getListPlans', context.state.options);
   });
-}
+};
 
 export const copy = (context, id) => {
-  api.post(API.COPY_PLAN + id).then((response) => {
-    document.location.href = encodeURI("plan/edit/"+response.data.id+"/"+response.data.title);
+  api.post(API.COPY_PLAN + id, null, { showLoader: true }).then((response) => {
+    document.location.href = encodeURI('plan/edit/' + response.data.id + '/' + response.data.title);
   });
-}
+};
 
 export const store = (context, data) => {
   return api.post(API.PLANS, data).then((response) => {
-    data.id = response.data.id
+    data.id = response.data.id;
     context.commit('SET_PLAN', data);
-    return  response;
+    return response;
   });
-}
+};
 
 export const update = (context, data) => {
   return api.put(`${API.PLANS}/${data.id}`, data).then((response) => {
     context.commit('SET_LOADING', false);
-    return  response;
+    return response;
   });
-}
+};
 
 export const show = (context, id) => {
   context.commit('SET_LOADING', true);
-  return api.edit(API.PLANS, id, {showLoader: true}).then((response) => {
+  return api.edit(API.PLANS, id, { showLoader: true }).then((response) => {
     context.commit('CLEAR_ERRORS_PLAN');
-    context.commit('SET_PLAN', {...response.data.data, submitLoader: false});
+    context.commit('SET_PLAN', { ...response.data.data, submitLoader: false });
     context.commit('SET_INDEX_COMPONENT');
     context.commit('SET_LOADING', false);
-    return  response;
+    return response;
   });
-}
+};
 
 export const getOptions = (context) => {
   return api.get(API.SETTINGS).then((response) => {
     let data = {};
-    response.data.data.forEach(item => {
+    response.data.data.forEach((item) => {
       data[item.key] = item.value;
     });
     context.commit('SET_OPTIONS', data);
-    return  response;
+    return response;
   });
-}
+};
 
 export const setOptions = (context, payload) => {
-  context.commit('SET_OPTIONS', payload)
-}
+  context.commit('SET_OPTIONS', payload);
+};
 
 export const setErrorsPlan = (context, payload) => {
-  context.commit('SET_ERRORS_PLAN', payload)
-}
+  context.commit('SET_ERRORS_PLAN', payload);
+};
 
 export const clearErrorsPlan = (context) => {
   context.commit('CLEAR_ERRORS_PLAN');
-}
+};
 
 export const clear = ({ commit, state }) => {
-  commit('SET_PLAN', state.default)
-}
+  commit('SET_PLAN', state.default);
+};
 
 export const addSignature = ({ commit }) => {
   commit('ADD_SIGNATURE');
@@ -93,7 +92,7 @@ export const removeSignature = (context, payload) => {
     context.commit('REMOVE_SIGNATURE', payload);
     return new Promise();
   }
-  return api.destroy(API.SIGNATURES, payload.id).then( (response) => {
+  return api.destroy(API.SIGNATURES, payload.id).then((response) => {
     context.commit('REMOVE_SIGNATURE', payload);
     return response;
   });
@@ -104,9 +103,9 @@ export const toggleSignature = (context, payload) => {
 };
 
 export const updateSignature = (context, payload) => {
-  return api.patch(API.SIGNATURES, payload.id,  payload).then((response) => {
+  return api.patch(API.SIGNATURES, payload.id, payload).then((response) => {
     context.commit('TOGGLE_SIGNATURE', payload);
-    context.commit('UPDATE_SIGNATURE', response.data );
+    context.commit('UPDATE_SIGNATURE', response.data);
     return response;
   });
 };
