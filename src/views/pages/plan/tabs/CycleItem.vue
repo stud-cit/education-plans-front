007282b23ff9624128b82fc9a2fa766a106776ae
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2023-07-17 14:42:58 +0300

    remove: checkCreditMethod

--- src/views/pages/plan/tabs/CycleItem.vue
+++ src/views/pages/plan/tabs/CycleItem.vue
@@ -3,49 +3,40 @@
     <v-row :class="['cycle-subject', 'cycle', cycleIndexError == item.id ? 'error' : '', 'ma-0', 'mb-1']">
       <v-col cols="8" class="pa-0">
         <select :disabled="cycleIndex != item.id" v-model="item.list_cycle_id">
-          <option 
-            style="color: black" 
-            :value="cycle.id" 
-            v-for="cycle in listCycles" 
-            :key="cycle.id"
-          >{{ cycle.title }}</option>
+          <option style="color: black" :value="cycle.id" v-for="cycle in listCycles" :key="cycle.id">
+            {{ cycle.title }}
+          </option>
         </select>
       </v-col>
       <v-col cols="1" class="pa-0">
         <v-tooltip bottom>
           <template v-slot:activator="{ on, attrs }">
-            <input 
-              type="checkbox" 
+            <input
+              type="checkbox"
               v-bind="attrs"
               v-on="on"
-              :disabled="cycleIndex != item.id" 
+              :disabled="cycleIndex != item.id"
               v-model="item.has_discipline"
-            >
+            />
           </template>
           <span>Цикл з навчальними дисциплінами</span>
         </v-tooltip>
       </v-col>
       <v-col cols="1" class="pa-0">
-        <input 
-          type="number" 
+        <input
+          type="number"
           min="0"
           class="credits"
-          :disabled="cycleIndex != item.id" 
+          :disabled="cycleIndex != item.id"
           v-model="item.credit"
           @input="checkCredit(parentItem, item)"
-        >
+        />
       </v-col>
       <v-col cols="1" class="pa-0"></v-col>
       <v-col class="pa-0 text-right">
         <v-tooltip bottom v-if="cycleIndex != item.id">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              small 
-              icon 
-              @click="cycleIndex = item.id" 
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn small icon @click="cycleIndex = item.id" v-bind="attrs" v-on="on">
               <v-icon>mdi-pencil</v-icon>
             </v-btn>
           </template>
@@ -53,49 +44,25 @@
         </v-tooltip>
         <v-tooltip bottom v-else>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              small 
-              icon 
-              @click="saveCycle(item)"
-              :disabled="item.title == ''"
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn small icon @click="saveCycle(item)" :disabled="item.title == ''" v-bind="attrs" v-on="on">
               <v-icon>mdi-floppy</v-icon>
             </v-btn>
           </template>
           <span>Зберегти</span>
         </v-tooltip>
-        <v-tooltip bottom v-if="allowedRoles([
-          ROLES.ID.admin,
-          ROLES.ID.root
-        ])">
+        <v-tooltip bottom v-if="allowedRoles([ROLES.ID.admin, ROLES.ID.root])">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              small 
-              icon 
-              @click="delCycle(item)"
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn small icon @click="delCycle(item)" v-bind="attrs" v-on="on">
               <v-icon>mdi-delete</v-icon>
             </v-btn>
           </template>
           <span>Видалити</span>
         </v-tooltip>
-        <v-menu
-          bottom
-          right
-        >
+        <v-menu bottom right>
           <template v-slot:activator="{ on: menu, attrs }">
             <v-tooltip bottom>
               <template v-slot:activator="{ on: tooltip }">
-                <v-btn
-                  icon
-                  small
-                  v-bind="attrs"
-                  v-on="{ ...tooltip, ...menu }"
-                >
+                <v-btn icon small v-bind="attrs" v-on="{ ...tooltip, ...menu }">
                   <v-icon>mdi-plus</v-icon>
                 </v-btn>
               </template>
@@ -103,10 +70,7 @@
             </v-tooltip>
           </template>
           <v-list>
-            <v-list-item link v-if="allowedRoles([
-              ROLES.ID.admin,
-              ROLES.ID.root
-            ])">
+            <v-list-item link v-if="allowedRoles([ROLES.ID.admin, ROLES.ID.root])">
               <v-list-item-title @click="addCycle(item)">Цикл</v-list-item-title>
             </v-list-item>
             <v-list-item link>
@@ -118,37 +82,24 @@
     </v-row>
 
     <v-row class="cycle-subject head ma-0 mb-1" v-if="item.subjects && item.subjects.length > 0">
-      <v-col cols="5" class="pa-0 text-left">
-        Назва предмету
-      </v-col>
-      <v-col class="pa-0">
-        Лекцій
-      </v-col>
-      <v-col class="pa-0">
-        Практичних
-      </v-col>
-      <v-col class="pa-0">
-        Лабораторних
-      </v-col>
-      <v-col class="pa-0">
-        Сума годин
-      </v-col>
-      <v-col class="pa-0">
-        Кредитів
-      </v-col>
-      <v-col class="pa-0">
-        Екзамени в семестрах
-      </v-col>
+      <v-col cols="5" class="pa-0 text-left"> Назва предмету </v-col>
+      <v-col class="pa-0"> Лекцій </v-col>
+      <v-col class="pa-0"> Практичних </v-col>
+      <v-col class="pa-0"> Лабораторних </v-col>
+      <v-col class="pa-0"> Сума годин </v-col>
+      <v-col class="pa-0"> Кредитів </v-col>
+      <v-col class="pa-0"> Екзамени в семестрах </v-col>
       <v-col class="pa-0"></v-col>
     </v-row>
 
     <SubjectItem
-      v-for="(subject, subjectIndex) in item.subjects" 
+      v-for="(subject, subjectIndex) in item.subjects"
       :key="'subject' + subjectIndex + indexComponent"
       :subject="subject"
-      :item="item"/>
+      :item="item"
+    />
 
-    <CycleItem 
+    <CycleItem
       :parentItem="item"
       v-bind:item="child"
       :index="subIndex"
@@ -160,103 +111,104 @@
       @addSubject="addSubject"
       @saveCycle="saveCycle"
       @editCycle="editCycle"
-      @delCycle="delCycle"/>
-
+      @delCycle="delCycle"
+    />
   </div>
 </template>
 <script>
 import SubjectItem from '@/views/pages/plan/tabs/SubjectItem.vue';
 
-import { ROLES } from "@/utils/constants"
-import RolesMixin from "@/mixins/RolesMixin";
+import { ROLES } from '@/utils/constants';
+import RolesMixin from '@/mixins/RolesMixin';
 
 export default {
-  name: "CycleItem",
+  name: 'CycleItem',
   components: {
-    SubjectItem
+    SubjectItem,
   },
   props: {
     item: {
       type: Object,
-      required: true
+      required: true,
     },
     index: {
       type: Number,
-      required: true
+      required: true,
     },
     indexComponent: {
       type: Number,
-      required: true
+      required: true,
     },
     parentItem: {
-      required: false
+      required: false,
     },
     data: {
       type: Object,
-      required: false
+      required: false,
     },
     cycles: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
   mixins: [RolesMixin],
   data() {
     return {
       ROLES,
       cycleIndex: null,
-      cycleIndexError: null
-    }
+      cycleIndexError: null,
+    };
   },
   mounted() {
     this.checkCredit(this.parentItem, this.item);
   },
   computed: {
     authUser() {
-      return JSON.parse(localStorage.getItem("user"));
+      return JSON.parse(localStorage.getItem('user'));
     },
     listCycles() {
-      return this.cycles.filter(cycle => {
-        if(this.item.list_cycle.general) {
+      return this.cycles.filter((cycle) => {
+        if (this.item.list_cycle.general) {
           return cycle.general;
         } else {
           return !cycle.general;
         }
-      })
-    }
+      });
+    },
   },
   methods: {
     checkCredit(parentItem, item) {
-      if(parentItem) {
+      if (parentItem) {
         this.checkCreditMethod(
-          parentItem, 
-          item, 
-          parentItem.credit, 
-          "Перевищена кількість кредитів в циклі " + parentItem.title + ": "
-        );
-      } else {
-        this.checkCreditMethod(
-          this.data, 
-          item, 
-          this.data.credits, 
-          "Перевищена загальна кількість кредитів: "
+          parentItem,
+          item,
+          parentItem.credit,
+          'Перевищена кількість кредитів в циклі ' + parentItem.title + ': ',
         );
       }
+      // else {
+      //   this.checkCreditMethod(
+      //     this.data,
+      //     item,
+      //     this.data.credits,
+      //     "Перевищена загальна кількість кредитів: "
+      //   );
+      // }
     },
     checkCreditMethod(data, item, limitCredit, message) {
       var cycles = data.cycles;
       var credints = [];
       (function flat(cycles) {
-        cycles.forEach(function(el) {
+        cycles.forEach(function (el) {
           if (Array.isArray(el)) flat(el);
           else credints.push(el.credit);
         });
       })(cycles);
       let sumCredits = credints.reduce((prev, curr) => +prev + +curr, 0);
-      if(limitCredit < sumCredits) {
+      if (limitCredit < sumCredits) {
         this.cycleIndexError = item.id;
-        if(this.cycleIndex == null) {
-          this.$store.dispatch('plans/setErrorsPlan', message + sumCredits + " із " + limitCredit);
+        if (this.cycleIndex == null) {
+          this.$store.dispatch('plans/setErrorsPlan', message + sumCredits + ' із ' + limitCredit);
         }
       } else {
         this.cycleIndexError = null;
@@ -264,58 +216,62 @@ export default {
       }
     },
     addSubject(item) {
-      this.$emit('addSubject', item)
+      this.$emit('addSubject', item);
     },
     addCycle(item) {
-      this.$emit('addCycle', item)
+      this.$emit('addCycle', item);
     },
     editCycle(item) {
-      this.$emit('editCycle', item)
+      this.$emit('editCycle', item);
     },
     saveCycle(item) {
-      this.$emit('saveCycle', item)
-      this.cycleIndex = null
+      this.$emit('saveCycle', item);
+      this.cycleIndex = null;
     },
     delCycle(item) {
-      this.$emit('delCycle', item)
+      this.$emit('delCycle', item);
     },
-  }
-}
+  },
+};
 </script>
 <style>
-  .cycle-subject {
-    width: 100%;
-    padding: 5px 10px 5px 10px;
-    background: #fff;
-    box-shadow: 0px 1px 2px #dedede;
-    border-radius: 5px;
-    margin-bottom: 2px;
-    font-size: 14px;
-    text-align: center;
-    display: flex;
-    align-items: center;
-  }
-  .cycle-subject.error, .cycle-subject.error input, .cycle-subject.error select, .cycle-subject.error i {
-    color: #fff !important;
-  }
-  .cycle-subject select:disabled {
-    color: #000 !important;
-  }
-  .cycle-subject.error select:disabled {
-    color: #fff !important;
-  }
-  .cycle-subject input, .cycle-subject select {
-    width: 90%;
-    text-align: center;
-  }
-  .cycle-subject.head {
-    text-align: center;
-    color: #9b9b9b;
-  }
-  .cycle-subject.cycle {
-    background: #f8f8f8;
-  }
-  input.credits {
-    padding-left: 14px;
-  }
+.cycle-subject {
+  width: 100%;
+  padding: 5px 10px 5px 10px;
+  background: #fff;
+  box-shadow: 0px 1px 2px #dedede;
+  border-radius: 5px;
+  margin-bottom: 2px;
+  font-size: 14px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+}
+.cycle-subject.error,
+.cycle-subject.error input,
+.cycle-subject.error select,
+.cycle-subject.error i {
+  color: #fff !important;
+}
+.cycle-subject select:disabled {
+  color: #000 !important;
+}
+.cycle-subject.error select:disabled {
+  color: #fff !important;
+}
+.cycle-subject input,
+.cycle-subject select {
+  width: 90%;
+  text-align: center;
+}
+.cycle-subject.head {
+  text-align: center;
+  color: #9b9b9b;
+}
+.cycle-subject.cycle {
+  background: #f8f8f8;
+}
+input.credits {
+  padding-left: 14px;
+}
 </style>
